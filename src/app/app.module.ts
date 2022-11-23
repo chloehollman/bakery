a45f589123985711781977b94630e735b2bb3921
chloehollman <chollman@byui.edu>
2022-11-22 16:44:08 -0800

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { OrderComponent } from './order/order.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    ContactComponent,
+    OrderComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
